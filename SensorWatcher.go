a23ec61d39d55cdfa3d2845071244faa134f933a
Kyle Kewley <kylek61294@gmail.com>
2016-08-21 10:59:43 -0700

    Preparing for adding the wiringPI library on the rpi

--- SensorWatcher.go
+++ SensorWatcher.go
@@ -1,12 +1,20 @@
 package main
 
+import (
+  "github.com/hugozhu/rpi"
+)
+
 type SensorWatcher struct {
+  Doors []DoorStatus
 }
 
 type DoorStatus struct {
   Name string
   Status string
   LastChanged int
+}
+
+func NewSensorWatcherWithConfig(config &Config) (error,*SensorWatcher) {
 }
 
 func (s *SensorWatcher) GetDoorStatus(doorName string) *DoorStatus {
